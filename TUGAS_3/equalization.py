a177b699dfe965cb7fcf9b35cd0989fe8a5a6afe
JuanBawias <114935957+JuanBawias@users.noreply.github.com>
2023-03-05 22:05:39 +0800

    Add files via upload

---
+++ TUGAS_3/equalization.py
@@ -0,0 +1,27 @@
+# Nama : Juan Sebastian Caesario Bawias
+# NIM  : F55121077
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# membaca citra
+img = cv2.imread('kucing.jpg',0)
+
+# membuat histogram equalization
+equ = cv2.equalizeHist(img)
+
+# menampilkan citra asli dan setelah histogram equalization
+cv2.imshow('Original',img)
+cv2.imshow('After Equalization',equ)
+
+# membuat histogram dari citra setelah histogram equalization
+hist,bins = np.histogram(equ.flatten(),256,[0,256])
+
+# menampilkan histogram
+plt.hist(equ.flatten(),256,[0,256])
+plt.xlim([0,256])
+plt.show()
+
+# menunggu input keyboard
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ TUGAS_3/main.py
@@ -0,0 +1,19 @@
+#Nama : Juan Sebastian Caesario Bawias
+#NIM  : F55121077
+import cv2
+import numpy as np
+
+pic1 = cv2.imread('juan1.jpg')
+pic2 = cv2.imread('juan2.jpg')
+
+gray_pic1 = cv2.cvtColor(pic1, cv2.COLOR_BGR2GRAY)
+gray_pic2 = cv2.cvtColor(pic2, cv2.COLOR_BGR2GRAY)
+
+diff = cv2.absdiff(gray_pic1, gray_pic2)
+
+eq_diff_pic = cv2.equalizeHist(diff)
+
+cv2.imshow('Difference Picture', diff)
+cv2.imshow('Histogram Equalization Difference Image', eq_diff_pic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
